2242e3e21dbb492a22c300075af3f117b0616979
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2023-01-18 10:57:33 -0300

    feat: add base to stack data structure

---
+++ structures/stack/stack-object.ts
@@ -0,0 +1,35 @@
+/* 
+    Stack Data Structure - based on object
+
+    A stack is an ordered collection of items with the LIFO(Last In First Out) principal. 
+    New additions or withdrawal of existing items occur at the same extremity.
+    The stack end is knowledge as top, while the opposite side is knowledge as base. 
+*/
+
+export default class Stack {
+    private items: Object
+    private count:  Number
+
+    constructor() {
+        this.items = {}
+        this.count = 0
+    }
+
+    // add element 
+    push(element) {}
+
+    // remove element from the top of the stack
+    pop() {}
+
+    // return element from the top of the stack
+    peek() {}
+
+    // return true if the stack is empyt
+    isEmpyt() {}
+
+    // remove all elements from the stack
+    clear() {}
+
+    // return the total of elements in the stack
+    size() {}
+}
